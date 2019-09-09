0fb649fc8ec868a16a042ea2a6e9dc78625a4edd
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-09 17:50:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def assign_rooms(attendees)
   
   room_assignments = []
   
-  attendees.each_with_index{|name, index| room_assignments << "Hello, #{name}!  You'll be assigned to room #{index+1}" }
+  attendees.each_with_index{|name, index| room_assignments << "Hello, #{name}!  You'll be assigned to room #{index+1}!" }
   
   room_assignments
 end
